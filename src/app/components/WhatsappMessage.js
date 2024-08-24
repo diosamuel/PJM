578930e461e9aaa71753e67380bc38baa34d1954
diosamuel <koding12sam@gmail.com>
2024-08-25 00:44:51 +0700

    fix copy again

--- src/app/components/WhatsappMessage.js
+++ src/app/components/WhatsappMessage.js
@@ -3,7 +3,7 @@ export default function WhatsappMessage(carts) {
 
 	let craft = 'Halo Bosq, saya ingin memesan :';
 	carts.forEach((cart) => {
-		craft += `\n${cart.quantity} ${cart.nama} = Rp${Number(cart.diskon).toLocaleString('id-ID')}`;
+		craft += `• \n${cart.quantity} ${cart.nama} = Rp${Number(cart.diskon).toLocaleString('id-ID')}`;
 	});
 	craft += `\n\nTOTAL: Rp${Number(total).toLocaleString('id-ID')}`;
 	craft += "\nApakah masih ada Bosq?"
